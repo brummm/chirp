a2fefede5c2080e6d084f0957d80f22ece465978
Rogerio Brum <rogeriobrum@gmail.com>
2023-06-08 16:18:53 -0300

    Adds better post listing to home

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,38 @@
+import { clerkClient } from "@clerk/nextjs";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const uniqueAuthorIds = new Set(posts.map((post) => post.authorId));
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: Array.from(uniqueAuthorIds),
+      })
+    ).map(({ profileImageUrl, firstName, username, id }) => ({
+      profileImageUrl,
+      firstName,
+      username,
+      id,
+    }));
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author not found",
+        });
+
+      return {
+        ...post,
+        author,
+      };
+    });
   }),
 });
