98297f24a0b38e10c85ad801cb4da789cf11eb67
Rogerio Brum <rogeriobrum@gmail.com>
2023-06-08 17:05:21 -0300

    Adds loading states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
+import { LoadingPage } from "~/components/LoadingSpinner";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
@@ -60,11 +61,23 @@ const PostView: React.FC<{ post: PostWithAuthor }> = ({ post }) => {
   );
 };
 
-const Home: NextPage = () => {
+const Feed = () => {
   const { data, isLoading } = api.posts.getAll.useQuery();
-  const user = useUser();
 
-  if (isLoading) return <>...Loading</>;
+  if (isLoading) return <LoadingPage />;
+  return (
+    <div className="flex flex-col">
+      {data?.map((post) => (
+        <PostView key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
+
+const Home: NextPage = () => {
+  const { isLoaded: userIsLoaded, isSignedIn } = useUser();
+
+  if (!userIsLoaded) return <div />;
 
   return (
     <>
@@ -75,15 +88,11 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex h-screen justify-center">
         <div className="w-full border-x border-slate-400 md:max-w-2xl">
-          <div className="bprder-slate-400 flex border-b p-4">
-            {!user.isSignedIn && <SignInButton />}
-            {user.isSignedIn && <CreatePostWizard />}
+          <div className="flex border-b border-slate-400 p-4">
+            {!isSignedIn && <SignInButton />}
+            {isSignedIn && <CreatePostWizard />}
           </div>
-          <div className="flex flex-col">
-            {data?.map((post) => (
-              <PostView key={post.id} post={post} />
-            ))}
-          </div>
+          <Feed />
         </div>
       </main>
     </>
